ffb97cc708e3afbd4a168cf4eddea3be9afc54a8
iammrverma <rajwebz2020@gmail.com>
2023-06-13 18:21:46 +0530

    removing repeadted imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Task from "./components/task";
-import Empty from "./components/empty";
-import NewTask from "./components/newTask";
 import Navbar from "./components/navbar";
 import Main from "./components/main";
-import NewTask from "./components/newTask";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
