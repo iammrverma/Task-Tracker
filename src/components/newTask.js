be90f2f706f823eda3a6b61cc6dda93ea779162f
iammrverma <rajwebz2020@gmail.com>
2023-06-13 18:53:13 +0530

    commit

--- src/components/newTask.js
+++ src/components/newTask.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { FaCheck, FaTimes, FaAngleDown } from "react-icons/fa";
+import { FaCheck, FaTimes } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
 
 
